0a60a400370269270cfd392a51e0b2a19ae69379
Carlota Pearl <carlota.pearl@gmail.com>
2018-08-21 13:55:56 -0400

    placed maxZoom on its on line

--- js/main.js
+++ js/main.js
@@ -78,7 +78,8 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: 'pk.eyJ1IjoiY2FlcDIwMTgiLCJhIjoiY2prbHBzbThzMHB1OTNxcGI1bDI1cDA0cyJ9.VkxbTRiKCw63K4cIrZM0Ag',    maxZoom: 18,
+    mapboxToken: 'pk.eyJ1IjoiY2FlcDIwMTgiLCJhIjoiY2prbHBzbThzMHB1OTNxcGI1bDI1cDA0cyJ9.VkxbTRiKCw63K4cIrZM0Ag',    
+    maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
       'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
